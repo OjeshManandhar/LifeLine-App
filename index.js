13487ec0d32319cc6020fff4cbd0200cbec7afc9
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2019-12-26 16:06:01 +0545

    change map api

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
-import Signup from './src/screens/Signup';
+import Map from './src/screens/Map';
 // import UserList from './src/components/UserList';
 import App from './App';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Map);
