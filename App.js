4596fef7d4e2f0e22d90dc3b23f049824083a2c0
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-02-09 09:14:25 +0545

    Add Stack navigation

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import {Router, Stack, Scene} from 'react-native-router-flux';
+import Map from './src/screens/Map';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
-import Map from './src/screens/Map';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => (
-  <Router>
-    <Stack key="root">
-      <Scene key="login" component={Login} title="Login" hideNavBar={true} />
-      <Scene key="signup" component={Signup} title="Signup" hideNavBar={true} />
-      <Scene key="home" component={Map} hideNavBar={true} />
-    </Stack>
-  </Router>
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="signup"
+        component={Signup}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="home"
+        component={Map}
+        options={{
+          title: 'Map',
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
 export default App;
