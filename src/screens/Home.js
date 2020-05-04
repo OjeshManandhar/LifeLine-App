d23f041ffeebb4d21c67a8756e17f001e822de06
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-04 18:34:44 +0545

    Add Home screen

---
+++ src/screens/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+function Home({navigation}) {
+  const userInfo = {
+    userId: 'lorem',
+    userName: 'DeadSkull',
+    phone: '1234567890',
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Home Screen</Text>
+      <Button
+        title="Go to Detail"
+        onPress={() =>
+          navigation.navigate('Detail', {
+            name: 'DeadSkull',
+            phone: '1234567890',
+          })
+        }
+      />
+      <Button
+        title="Go to Map"
+        onPress={() =>
+          navigation.navigate('Map', {
+            userInfo: JSON.stringify(userInfo),
+          })
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  textStyle: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
+
+export default Home;
