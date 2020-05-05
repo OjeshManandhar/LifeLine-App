21b2b3ecb698ccc7335dd8df8493bb8387457429
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-05 16:11:12 +0545

    remove unnecessary import

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
 // packages
 import MapboxGL from '@react-native-mapbox-gl/maps';
