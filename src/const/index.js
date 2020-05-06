6c3aa896d4e35b3de4b9c49b508f1cbe318a18db
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-06 18:55:42 +0545

    constants for alert messages

---
+++ src/const/index.js
@@ -0,0 +1,18 @@
+export const LoginText = {
+  Signup: {
+    title: 'Signup ?',
+    detail: `Please contact the admin to register you to the App.
+Thank You!`,
+  },
+  Error: {
+    title: 'Login Error !',
+    detail: `Please enter both contact and password.
+Try again!`,
+  },
+  Server: {
+    title: 'Server Error!',
+    detail1: 'Couldnot connect with server ...',
+    detail2: `Invalid Contact or Password!
+Try Again.`,
+  },
+};
