33bfb0d645cf858a5f15598528161047c6d29dd7
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-06 18:56:20 +0545

    Add states for alert

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -3,38 +3,77 @@ import axios from 'axios';
 
 //const
 import {Ip} from '../const/ip';
+import {LoginText} from '../const';
 
 const useLogin = navigation => {
   const [name, setName] = useState('');
   const [pass, setPass] = useState('');
   const [token, setToken] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [title, setTitle] = useState(LoginText.Signup.title);
+  const [detail, setDetail] = useState(LoginText.Signup.detail);
+  const [disable, setDisable] = useState(false);
 
   function handleLogin() {
     var session_url = Ip.driver_login;
     var uname = name;
     var password = pass;
-    axios
-      .post(
-        session_url,
-        {},
-        {
-          auth: {
-            username: uname,
-            password: password,
-          },
-        },
-      )
-      .then(function(response) {
-        setToken(response.data);
-        console.log(token);
-        navigation.navigate('Home');
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    setDisable(true);
+    if (!uname || !password) {
+      setVisible(true);
+      setDisable(false);
+      setTitle(LoginText.Error.title);
+      setDetail(LoginText.Error.detail);
+    } else {
+      try {
+        axios
+          .post(
+            session_url,
+            {},
+            {
+              auth: {
+                username: uname,
+                password: password,
+              },
+            },
+          )
+          .then(function(response) {
+            setToken(response.data);
+            console.log(token);
+            navigation.navigate('Home');
+          })
+          .catch(function(error) {
+            console.log(error);
+            setVisible(true);
+            setTitle(LoginText.Server.title);
+            setDetail(LoginText.Server.detail2);
+          });
+        throw 'error';
+      } catch (error) {
+        console.log('error caught: ', error);
+        setVisible(true);
+        setTitle(LoginText.Server.title);
+        setDetail(LoginText.Server.detail1);
+      } finally {
+        setDisable(false);
+      }
+    }
   }
 
-  return {name, setName, pass, setPass, handleLogin};
+  return {
+    name,
+    setName,
+    pass,
+    setPass,
+    visible,
+    setVisible,
+    title,
+    setTitle,
+    detail,
+    setDetail,
+    handleLogin,
+    disable,
+  };
 };
 
 export default useLogin;
