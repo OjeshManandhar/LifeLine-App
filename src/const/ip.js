929875d385e6e7bd75f794f02341d8f13e625f88
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-07 12:29:38 +0545

    Add url for driver image

--- src/const/ip.js
+++ src/const/ip.js
@@ -2,4 +2,5 @@ export const Ip = {
   driver: 'http://192.168.0.117:5000/driver',
   driver_signup: 'http://192.168.0.117:5000/driver_signup',
   driver_login: 'http://192.168.0.117:5000/driver_login',
+  driver_pic: 'http://192.168.0.117:5000/get_driver_pic/',
 };
