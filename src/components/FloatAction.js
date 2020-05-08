7c77c2e3dfc72db7c64c8682505315181a9c892d
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-08 11:04:47 +0545

    Add float action component

---
+++ src/components/FloatAction.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const FloatAction = () => {
+  return (
+    <ActionButton buttonColor="rgba(231,76,60,1)" position="right">
+      <ActionButton.Item
+        buttonColor="#9b59b6"
+        title="New Task"
+        onPress={() => console.log('notes tapped!')}>
+        <Icon name="md-create" style={styles.actionButtonIcon} />
+      </ActionButton.Item>
+      <ActionButton.Item
+        buttonColor="#3498db"
+        title="Notifications"
+        onPress={() => {}}>
+        <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+      </ActionButton.Item>
+      <ActionButton.Item
+        buttonColor="#1abc9c"
+        title="All Tasks"
+        onPress={() => {}}>
+        <Icon name="md-done-all" style={styles.actionButtonIcon} />
+      </ActionButton.Item>
+    </ActionButton>
+  );
+};
+
+const styles = StyleSheet.create({
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
+
+export default FloatAction;
